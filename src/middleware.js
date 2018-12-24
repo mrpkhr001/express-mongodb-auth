8cf226641b66f4f0128e2bdf1d04aed9388f04d0
Rajendra M <techemail.rajesh@gmail.com>
2018-12-24 06:49:45 +0000

    Updated cors configuration

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,11 @@
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const middleware  = function(app){
-    app.use(bodyParser.json({type: '*/*'}));
     app.use(morgan('combined'));
+    app.use(cors());
+    app.use(bodyParser.json({type: '*/*'}));
 };
 
 
